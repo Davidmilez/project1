f312ddedd95b3fe8f1a92b452238f100975f0670
David <David4mies@gmail.com>
2024-06-24 21:06:51 +0200

    hello

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -6,9 +6,7 @@ public class Main {
     public static void main(String[] args) {
 //        Write a method that takes a list of numbers (Integer, Double, etc.)as input
 //        and calculates the average of those numbers. Use an upper bounded wildcard
-//        to ensure that the method accepts lists of any type that extends Number
-        List<>
-
-
+//        to ensure that the method accepts lists of any type that extends Num
+        System.out.println("Hello github");
     }
 }
